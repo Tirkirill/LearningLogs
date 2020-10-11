8ce234b7c911c69b6ddf3c92d3b1b523ebecd072
Tirkirill <sherbakirill@mail.ru>
2020-10-11 21:28:37 +0300

    Инициализация

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+
+
+module.exports = {
+    entry: "./app/index.jsx",
+    output:{
+        path: path.resolve(__dirname, './public'),
+        publicPath: '/public/',
+        filename: "bundle.js"
+    },
+    module:{
+        rules:[
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options:{
+                    presets:["@babel/preset-env", "@babel/preset-react"]
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }]
+            },
+            {
+                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]'
+                }
+            }
+        ],
+    },
+}
